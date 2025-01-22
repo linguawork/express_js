7d0ad8715eb94933bceea2d25b3bcab739fc30db
linguawork <mixedworldcitizen@gmail.com>
2025-01-22 12:16:08 +0100

    16:49 - 20:10, создали модель Пост из схемы и импортировали, из постмана отправили запрос post по типу модели в формате JSON объект и в ответ отправили этот же объект, поигрались с ошибками

---
+++ Post.js
@@ -0,0 +1,18 @@
+//16:50
+//модель данных для Поста с содержанием и картинкой
+
+import mongoose from "mongoose";
+
+
+//модель будет построена на схеме
+const Post = new mongoose.Schema(
+    //required field are a must to fill
+    {
+        author: {type:String, required: true }, 
+        title: {type:String, required: true }, 
+        content: {type:String, required: true }, 
+        picture: {type:String}, 
+    }
+)
+
+export default mongoose.model('Post', Post)
--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import express from 'express'
 //14:48db framework
 import mongoose from 'mongoose'
 
+//19:00
+import Post from './Post.js'
+import { triggerAsyncId } from 'async_hooks'
+
 //DB. user and admin are automatically replaced in the URL in MongoDB
 const DB_URL = 'mongodb+srv://admin:admin@clusterforexpressulbi.ng1yv.mongodb.net/?retryWrites=true&w=majority&appName=ClusterForExpressUlbi'
 
@@ -15,9 +19,10 @@ const app = express()
 app.use(express.json())
 
 //endpoint get: 1st param: address, second - callback f
-app.post('/', (req, res) => {
+app.post('/', async (req, res) => {
 
-            /*11:35 GET and printing QUERY PARAMETERS
+            /*
+            11:35 GET and printing QUERY PARAMETERS
                 // query parameters при запросе от клиента
                 // ?test=123&query=asfasf&third=addsa 
                 console.log(req.query)
@@ -44,24 +49,40 @@ app.post('/', (req, res) => {
         { name: 'Тест', username: 'areggie42' }
     */
     console.log(req.body)
+    console.log(req.body.name)
+    /*
+    Если намеренно в Постмане не отправить требуемый ключ
+    то постман ничего не отправит.
+    Для решения этой проблемы лучше поместить код в блок try-catch
+    и тогда в постмане можно увидеть указание на ошибку
+
+    Хотя без блока в консоли с запущенным сервером можно увидеть 
+    намек на ошибку
+    */
+    try {
+        
+        //19:31запрос от клиента (Postman)
+        //19:01 Import Post model
+        const {author, title, content, picture} = req.body
+        const post = await Post.create({author, title, content, picture})
     
+            //ответ клиенту тоже в Postman(19:31)
+            //res.status(200).json('Сервер работает ')
+        res.status(200).json(post) // возврат поста
+        /*
+            Когда отправляем post запрос из Postman
+            In Postman можно видеть возврат Post объекта
+    
+            И в МонгоДБ нажимаем Browse collections in the
+            created cluster, the cluster is written in the URL
+            и можно увидеть
+            2 документа сохраненных в базе данных
+            то есть два объекта
+        */
+    } catch (error) {
+        res.status(500).json(error) 
+    }
 
-    //ответ клиенту
-    res.status(200).json('Сервер работает ')
-                /*
-                    При каждом изменении нужно перезапускать сервер.
-                    Чтобы не перезапускать нужно ввести команду:
-                    npm i -D nodemon
-
-
-                    "scripts": {
-                    "start": "node index.js",
-                    "dev": "nodemon index.js"
-
-                    }
-                    
-                    Для запуска в режиме разработки: npm run dev
-                */
 
 })
 
